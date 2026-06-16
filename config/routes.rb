c43abb891043a3f7f11ac8dacf36d9f2b9e28669
Alexander Hardenberg <alexandergvhardenberg@gmail.com>
2026-06-16 13:20:12 +0200

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
   get "offers/index"
->>>>>>> offer-actions
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
