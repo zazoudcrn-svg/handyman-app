f5e0d4bc9e9a963329020f1e00822f68c4846831
desiree kaidi <desireekaidi@gmail.com>
2026-06-16 13:04:05 +0200

    As a customer AND contractor, I can visit the landing page to see the app’s value proposition and create an account or log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  before_action :authenticate_user!
+
+  private
+
+  # Overwriting the Devise redirect path after a successful sign-in
+  def after_sign_in_path_for(resource)
+    # Redirect directly to the user dashboard page
+    dashboard_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "pages#home"
+
+  get "dashboard", to: "dashboards#show", as: :dashboard
 end
