657b742a5b457244893c9d5e69d40cf69e93d850
Alexander Hardenberg <alexandergvhardenberg@gmail.com>
2026-06-16 12:02:11 +0200

    merged master again

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,11 +1,6 @@
 class Offer < ApplicationRecord
-<<<<<<< HEAD
-  belongs_to :listing
-  belongs_to :user
-=======
   belongs_to :user
   belongs_to :listing
   has_many :messages
   has_one :booking
->>>>>>> master
 end
