ec01171d6dad98947a2d0881f200c920684c2851
desiree kaidi <desireekaidi@gmail.com>
2026-06-16 15:58:34 +0200

    test: authenticate user for dashboard show test

--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class DashboardsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should get show" do
+    user = users(:one)
+    sign_in user
+
     get dashboard_url
     assert_response :success
   end
