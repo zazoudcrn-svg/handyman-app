6fb12a29d7f0aa7511d7e11406c69a00eacb2173
Alexander Hardenberg <alexandergvhardenberg@gmail.com>
2026-06-16 13:01:24 +0200

    Fix offers controller test to use nested route

--- test/controllers/offers_controller_test.rb
+++ test/controllers/offers_controller_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class OffersControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get offers_index_url
+    listing = listings(:one)
+    get listing_offers_url(listing)
     assert_response :success
   end
 end
