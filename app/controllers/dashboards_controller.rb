773b02ac9c819a01b1106f087be7326332ff092f
desiree kaidi <desireekaidi@gmail.com>
2026-06-16 14:22:38 +0200

    dashboard for customer and contractor done

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,16 @@
 class DashboardsController < ApplicationController
   def show
+    # Check if the logged-in user has a contractor profile setup
+    if current_user.contractor_profile.present?
+      # 1. Contractor Logic
+      # For now, we load all listings so they have something to see
+      @listings = Listing.all
+      render :contractor_show
+    else
+      # 2. Customer Logic
+      # Fetch only the listings created by this specific customer
+      @listings = current_user.listings
+      render :customer_show
+    end
   end
 end
