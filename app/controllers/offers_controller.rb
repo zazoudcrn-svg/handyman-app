93a0a11eab6e90f1482f62c5a865c166197ec34b
Alexander Hardenberg <alexandergvhardenberg@gmail.com>
2026-06-17 16:38:27 +0200

    Add declined offers page and redirect customer to index on decline

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,8 +1,14 @@
 class OffersController < ApplicationController
   def index
-    @listing = Listing.find(params[:listing_id])
-    @offers = @listing.offers
+  @listing = Listing.find(params[:listing_id])
+  @offers = @listing.offers.where(offer_status: [ nil, "pending", "accepted" ])
   end
+
+  def declined
+  @listing = Listing.find(params[:listing_id])
+  @offers = @listing.offers.where(offer_status: "declined")
+  end
+
   def show
     @listing = Listing.find(params[:listing_id])
     @offer = Offer.find(params[:id])
@@ -21,6 +27,6 @@ class OffersController < ApplicationController
     @listing = Listing.find(params[:listing_id])
     @offer = Offer.find(params[:id])
     @offer.update(offer_status: "declined")
-    redirect_to listing_offer_path(@listing, @offer), notice: "Offer declined!"
+    redirect_to listing_offers_path(@listing), notice: "The offer has been declined" # CHANGED
   end
 end
