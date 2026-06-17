38087a5ffe1eaf15b5846477d4bcdff3eacb3e0a
desiree kaidi <desireekaidi@gmail.com>
2026-06-17 12:52:33 +0200

    fixed bugs

--- test/controllers/listings_controller_test.rb
+++ test/controllers/listings_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class ListingsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
   test "should get new" do
     get new_listing_url
     assert_response :success
