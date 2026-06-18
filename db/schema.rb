e6da61d0be0b45c186df447110479a59a0cd0560
rupa yadav <rupayadav0890@gmail.com>
2026-06-18 23:57:57 +0200

    WIP: Contractor Bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_17_152707) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_18_151028) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_17_152707) do
   end
 
   create_table "bookings", force: :cascade do |t|
+    t.string "accepted_by"
     t.string "booking_status", default: "pending"
     t.text "cancellation_note"
     t.string "cancelled_by"
