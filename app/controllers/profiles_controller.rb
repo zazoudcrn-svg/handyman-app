fd8c435616febd04ecce036fec061e23bcdad07a
Xavier Ducourneau <xavierdcrn@gmail.com>
2026-06-22 16:19:22 +0200

    worked on show pages and chat

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,9 +2,19 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user
   before_action :set_contractor_profile
+  before_action :authorize_own_profile!, only: [:edit, :update]
 
   def show
-    @reviews = @user.reviews_received.order(created_at: :desc)
+    @is_own_profile = current_user == @user
+    @reviews = @user.reviews_received.includes(:user).order(created_at: :desc)
+    @avg_rating = @reviews.average(:rating)&.round(1)
+
+    if @user.contractor?
+      @jobs_completed = @user.offers.joins(:booking)
+                             .where(bookings: { booking_status: "completed" }).count
+    else
+      @jobs_posted = @user.listings.count
+    end
   end
 
   def edit
@@ -25,12 +35,19 @@ class ProfilesController < ApplicationController
   end
 
   def set_contractor_profile
-    @contractor_profile = @user.contractor_profile if @user.role == "contractor"
+    @contractor_profile = @user.contractor_profile if @user.contractor?
+  end
+
+  def authorize_own_profile!
+    unless current_user == @user
+      redirect_to profile_path(@user), alert: "You are not authorized to do that."
+    end
   end
 
   def user_params
     params.require(:user).permit(
       :first_name, :last_name, :email, :street, :city, :postcode, :country,
+      :bio, :avatar,
       contractor_profile_attributes: [
         :id, :business_name, :street, :city, :postcode, :country,
         :travel_radius, :start_time, :end_time, :weekday_availability, :google_business_profile
