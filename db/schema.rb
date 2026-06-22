6c9601b90053ce7168c8c79c497961fe65deb639
Xavier Ducourneau <xavierdcrn@gmail.com>
2026-06-22 15:17:32 +0200

    add bio to users migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_22_094809) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_22_131646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -302,6 +302,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_22_094809) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.text "bio"
     t.string "city"
     t.string "country"
     t.datetime "created_at", null: false
